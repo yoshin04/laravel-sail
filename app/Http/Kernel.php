0928a455c4d7295ad0e96422cb68b97b62897223
Yoshin <taigaby0411@gmail.com>
2022-08-21 10:18:24 +0900

    ログイン機能の実装

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'sample' => \App\Http\Middleware\SampleMiddleware::class,
     ];
 }
